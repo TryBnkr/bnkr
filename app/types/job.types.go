d146dab9a2cfc1e127a5a9ccf8e325b011025132
Mohammed Al-Mahdawi <mohammed@al-mahdawi.is>
2021-06-30 20:54:00 +0300

    WIP

--- app/types/job.types.go
+++ app/types/job.types.go
@@ -11,7 +11,9 @@ type NewJobDTO struct {
 	File      string             `db:"file"`
 	Status    string             `db:"status"`
 	Backup    int                `db:"backup"`
-	CreatedAt pgtype.Timestamptz `db:"created_at"`
+	CreatedAt        pgtype.Timestamptz `db:"created_at"`
+	UpdatedAt        pgtype.Timestamptz `db:"updated_at"`
+	DeletedAt        pgtype.Timestamptz `db:"deleted_at"`
 }
 
 type SmallJob struct {
